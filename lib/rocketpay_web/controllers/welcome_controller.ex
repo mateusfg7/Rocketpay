5e095ef985aea96729ebf3c6a16a882268648c86
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-22 20:48:44 -0300

    create business rules for welcome controller

--- lib/rocketpay_web/controllers/welcome_controller.ex
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -1,7 +1,22 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(conn, _params) do
-    text(conn, "Welcome to the Rocketpay API")
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number #{result}"})
+  end
+  defp handle_response({:error, reson}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reson)
   end
 end
