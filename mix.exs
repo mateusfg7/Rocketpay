9f934218bd1547262abc2303d5f1027bb21473a2
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-25 11:40:49 -0300

    install bcrypt dependencie

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Rocketpay.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: true}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: true},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
