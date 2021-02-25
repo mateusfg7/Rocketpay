66495ad1762a3fb0504fe4b361ca1bc026064e96
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-25 08:36:32 -0300

    update creado config
    runtime: true

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Rocketpay.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: true}
     ]
   end
 
