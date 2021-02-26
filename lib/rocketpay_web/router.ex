0d9412cbdd6660058792f45ac708e88eab072313
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-26 20:26:39 -0300

    create withdraw route

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -13,6 +13,8 @@ defmodule RocketpayWeb.Router do
     post "/users", UsersController, :create
 
     post "/accounts/:id/deposit", AccountsController, :deposit
+
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
   end
 
   # Enables LiveDashboard only for development
