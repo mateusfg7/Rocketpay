136a325d672b951c800481135b241d43036cea15
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-26 20:10:16 -0300

    extend 400.json error template to a second case on error view

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -24,6 +24,10 @@ defmodule RocketpayWeb.ErrorView do
     }
   end
 
+  def render("400.json", %{result: message}) do
+    %{message: message}
+  end
+
   defp translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Enum.reduce(opts, msg, fn {key, value}, acc ->
