025567aacf5bbb32dda7e5f168b6abb449371823
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-03-03 16:37:39 -0300

    create tests for RocketpayWeb.UsersView

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias RocketpayWeb.UsersView
+  alias Rocketpay.{Account, User}
+
+  test "renders create.json" do
+    params = %{
+      name: "Mateus",
+      password: "123456",
+      nickname: "mateusfg7",
+      email: "mateusfg7@protonmail.com",
+      age: 18
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Mateus",
+        nickname: "mateusfg7"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
